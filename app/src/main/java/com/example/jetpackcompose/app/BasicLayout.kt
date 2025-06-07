2455bdac1987ce2eeaaa634a654b2d16447d47c1
sivaprakash.rangasamy <sivaprakash.rangasamy@ivymobility.com>
2025-06-07 16:53:58 +0530

    Filter option

--- app/src/main/java/com/example/jetpackcompose/app/BasicLayout.kt
+++ app/src/main/java/com/example/jetpackcompose/app/BasicLayout.kt
@@ -2,6 +2,7 @@ package com.example.jetpackcompose.app
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
@@ -29,11 +30,17 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationRail
@@ -43,20 +50,31 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastFilter
 import com.example.jetpackcompose.R
 import com.example.jetpackcompose.data.Profile
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -75,10 +93,13 @@ class BasicLayout : ComponentActivity() {
     }
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun MyApp() {
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
         Scaffold(
-            bottomBar = { Navigation() }
+            bottomBar = { Navigation() },
+           // topBar = {SmallAppBar(scrollBehavior,"Home")}
         ) { padding ->
             HomeScreen(Modifier.padding(padding))
         }
@@ -98,13 +119,77 @@ class BasicLayout : ComponentActivity() {
     }
 
 
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MediumTopAppBar(scrollBehavior: TopAppBarScrollBehavior) {
+        MediumTopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors( // use correct API
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary),
+            title = { Text("Small top App Bar", maxLines = 1, overflow = TextOverflow.Ellipsis) },
+            navigationIcon = {
+                IconButton(onClick = { /* handle back */ }) {
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
+            },
+            actions = {
+                IconButton(onClick = { /* handle menu */ }) {
+                    Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+                }
+            },
+            scrollBehavior = scrollBehavior
+        )
+    }
+
+
+
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SmallAppBar(scrollBehavior: TopAppBarScrollBehavior,title: String) {
+        val context = LocalContext.current
+        TopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+
+                IconButton(onClick = {  Toast.makeText(context, "Back clicked", Toast.LENGTH_SHORT).show() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            },
+            actions = {
+                IconButton(onClick = { Toast.makeText(context, "Menu clicked", Toast.LENGTH_SHORT).show() }) {
+                    Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+                }
+                IconButton(onClick = { Toast.makeText(context, "Profile clicked", Toast.LENGTH_SHORT).show() }) {
+                    Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "Menu")
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            scrollBehavior = scrollBehavior
+        )
+    }
+
+
     @Composable
     fun SearchBar(
-        modifier: Modifier = Modifier
-    ) {
+        modifier: Modifier = Modifier, query: String, onQueryChanged: (String) -> Unit) {
         TextField(
-            value = "",
-            onValueChange = {},
+            value = query, onValueChange = onQueryChanged,
             leadingIcon = {
                 Icon(
                     imageVector = Icons.Default.Search,
@@ -153,17 +238,20 @@ class BasicLayout : ComponentActivity() {
 
     @Composable
     fun HomeScreen(modifier: Modifier = Modifier) {
+        var searchText by remember { mutableStateOf("") }
         Column(
             modifier
                 .verticalScroll(rememberScrollState())
         ) {
             Spacer(Modifier.height(16.dp))
-            SearchBar(Modifier.padding(horizontal = 16.dp))
+            SearchBar(Modifier.padding(horizontal = 16.dp),
+                query=searchText,
+                onQueryChanged = { searchText = it })
             HomeSection(title = R.string.body) {
-                Body()
+                Body(searchText)
             }
             HomeSection(title = R.string.favorite) {
-                Favorite()
+                Favorite(searchText)
             }
             Spacer(Modifier.height(16.dp))
         }
@@ -213,7 +301,7 @@ class BasicLayout : ComponentActivity() {
 
 
     @Composable
-    fun Favorite() {
+    fun Favorite(searchText: String) {
         val data = mutableListOf<Profile>()
         data.add(
             Profile(
@@ -222,11 +310,11 @@ class BasicLayout : ComponentActivity() {
                 image = R.drawable.profile
             )
         )
-        data.add(Profile(description = "Challange", profileUrl = "", image = R.drawable.profile))
+        data.add(Profile(description = "Challenge", profileUrl = "", image = R.drawable.profile))
         data.add(Profile(description = "Massage", profileUrl = "", image = R.drawable.profile))
         data.add(Profile(description = "Nature", profileUrl = "", image = R.drawable.profile))
         data.add(Profile(description = "Food", profileUrl = "", image = R.drawable.profile))
-
+        val fitered=data.fastFilter { it.description.contains(searchText) }
         LazyHorizontalGrid(
             rows = GridCells.Fixed(2),
             contentPadding = PaddingValues(horizontal = 16.dp),
@@ -234,26 +322,27 @@ class BasicLayout : ComponentActivity() {
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.height(168.dp)
         ) {
-            items(data) { list -> FavoriteCollectionCard(list, modifier = Modifier.height(80.dp)) }
+            items(fitered) { it -> FavoriteCollectionCard(it, modifier = Modifier.height(80.dp)) }
         }
     }
 
     @Composable
-    fun Body() {
+    fun Body(searchText: String) {
         val data = mutableListOf<Profile>()
         data.add(Profile(description = "Yoga 1", profileUrl = "", image = R.drawable.stone))
         data.add(Profile(description = "Yoga 2", profileUrl = "", image = R.drawable.stone))
         data.add(Profile(description = "Yoga 3", profileUrl = "", image = R.drawable.stone))
         data.add(Profile(description = "Yoga 4", profileUrl = "", image = R.drawable.stone))
         data.add(Profile(description = "Yoga 5", profileUrl = "", image = R.drawable.stone))
+        val fitered=data.fastFilter { it.description.contains(searchText) }
         LazyRow(
             horizontalArrangement = Arrangement.spacedBy(2.dp),
             contentPadding = PaddingValues(horizontal = 4.dp),
         ) {
-            items(data) { data ->
+            items(fitered) { it ->
                 AlignYourBodyElement(
-                    data.image,
-                    data.description,
+                    it.image,
+                    it.description,
                     modifier = Modifier.padding(8.dp)
                 )
             }
